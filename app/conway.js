5ccece1749457453cd62da437b0a489aa96cd87b
hell03610 <emma@becodemyfriend.com>
2012-12-11 17:55:32 +0100

    bigger board

--- app/conway.js
+++ app/conway.js
@@ -1,14 +1,13 @@
 var conwaySketch = function($p) {
 
-    var w = 400,
+    var w = 600,
         h = 400;
-    var rows = 10,
-        columns = 10;
-    var diameter = (w / columns);
+    var diameterX = (w / columns);
+    var diameterY = (h / rows);
     
     function setup() {
         $p.size(w, h);
-        $p.strokeWeight(5);
+        $p.strokeWeight(2);
         $p.frameRate(2);
     }
     $p.setup = setup;
@@ -18,19 +17,19 @@ var conwaySketch = function($p) {
         $p.fill(100, 121, 184);
         $p.stroke(255);
 
-        var X = diameter * 0.5,
-            Y = diameter * 0.5;
+        var X = diameterX * 0.5,
+            Y = diameterY * 0.5;
     
         for(var i=0;i<rows;i++){
           for(var j=0;j<columns;j++){
               var cell = board.get(i,j);
               if(cell && cell.isAlive()){
-                $p.ellipse(X, Y, diameter, diameter);
+                $p.ellipse(X, Y, diameterX, diameterY);
               }
-              Y += diameter;
+              Y += diameterY;
           }
-          X += diameter; 
-          Y = diameter * 0.5;
+          X += diameterX; 
+          Y = diameterY * 0.5;
         }
 
         board.nextGeneration();
--- app/init.js
+++ app/init.js
@@ -1,4 +1,7 @@
+rows = 20;
+columns = 20;
+   
 var canvas = document.getElementById("canvas1");
-board = new Board(10,10);
+board = new Board(rows,columns);
 board.firstGeneration();
 var p = new Processing(canvas, conwaySketch);
