038d20275ed94dd688259be28e56c96caf15d2f2
hell03610 <emma@becodemyfriend.com>
2012-12-11 21:44:15 +0100

    generation and number of cells showed. posibility of pausing/resuming

--- app/conway.js
+++ app/conway.js
@@ -4,6 +4,11 @@ var conwaySketch = function($p) {
         h = 400;
     var diameterX = (w / columns);
     var diameterY = (h / rows);
+    var generation = 0;
+    var totalCells = 0;
+    var $generation = $('#generation');
+    var $aliveCells = $('#alive_cells');
+    
     
     function setup() {
         $p.size(w, h);
@@ -12,19 +17,27 @@ var conwaySketch = function($p) {
     }
     $p.setup = setup;
 
-    function draw() {
+    function initBackground(){
         $p.background(100);
         $p.fill(100, 121, 184);
         $p.stroke(255);
+    }
 
+    function draw() {
+    
+        initBackground();
+          
         var X = diameterX * 0.5,
             Y = diameterY * 0.5;
+
+        var totalCells = 0;
     
         for(var i=0;i<rows;i++){
           for(var j=0;j<columns;j++){
               var cell = board.get(i,j);
               if(cell && cell.isAlive()){
                 $p.ellipse(X, Y, diameterX, diameterY);
+                totalCells++;
               }
               Y += diameterY;
           }
@@ -33,7 +46,9 @@ var conwaySketch = function($p) {
         }
 
         board.nextGeneration();
-
+        generation++;
+        $generation.text(generation);
+        $aliveCells.text(totalCells);
     }
     $p.draw = draw;
 };
--- app/init.js
+++ app/init.js
@@ -2,6 +2,8 @@ rows = 0;
 columns = 0;
 board = null;
 animation = null;
+isPaused = false;
+processingInstance = null;
 
 function init(){
 	if(animation) animation.exit();
@@ -13,3 +15,28 @@ function init(){
 	board.firstGeneration();
 	animation = new Processing(canvas, conwaySketch);
 }
+
+
+function pause(){
+	if(isPaused) {
+		switchSketchState(true);
+		isPaused = false;
+		$('#stop_button').text('Pause');
+	}
+	else{
+		switchSketchState(false);
+		isPaused = true;
+		$('#stop_button').text('Resume');
+	}
+}
+
+ function switchSketchState(toOn) {
+    if (!processingInstance) {
+        processingInstance = Processing.getInstanceById('canvas1');
+    }
+    if (toOn) {
+       processingInstance.loop();  
+    } else {
+       processingInstance.noLoop();
+    }
+ }
